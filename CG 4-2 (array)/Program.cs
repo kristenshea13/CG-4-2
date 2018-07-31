cd628f10af2c89d5ad32fb41a33c64cb88121088
Kristen Shea <kstar61@live.com>
2018-07-30 22:16:07 -0500

    array

--- CG 4-2 (array)/Program.cs
+++ CG 4-2 (array)/Program.cs
@@ -6,11 +6,10 @@ namespace CG_4_2__array_
     {
         static void Main(string[] args)
         {
-            //name array, declare as integer and enter all numbers in array
-            int[] newArray = new int[6] 
-                { 1, 1, 2, 3, 5, 8 };
+            //name array, declare as integer and enter all numbers in array.
+            int[] newArray = new int[6] { 1, 1, 2, 3, 5, 8 };
 
-            //to copy each number into the variable "number" and then print it to the console
+            //to copy each number into the variable "number" and then print it to the console.
             foreach (var number in newArray)
             {
                 Console.WriteLine(number);
