4ba29ebd4fa6cbaf19c9d362d4f3adc135cc1433
Kristen Shea <kstar61@live.com>
2018-07-30 21:03:56 -0500

    Add project files.

---
+++ CG 4-2 (array)/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CG_4_2__array_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
